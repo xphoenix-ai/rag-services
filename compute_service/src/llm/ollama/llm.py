aaccca3ac93a98d3c2a42e8686846203f37579d9
kasun <kasun.imesha@toku.co>
2024-07-04 18:47:05 +0530

    added ollama llm class

---
+++ compute_service/src/llm/ollama/llm.py
@@ -0,0 +1,67 @@
+import os
+import torch
+import ollama
+from ollama import generate, chat, show, pull
+
+
+from src.llm.llm_base import LLMBase
+
+
+class LLM(LLMBase):
+    def __init__(
+        self, 
+        model_path="phi3",
+        keep_alive="5m",
+        tokenize_with_chat_template=True
+        ):
+        self.model = None
+        self.keep_alive = keep_alive
+        self.tokenize_with_chat_template = tokenize_with_chat_template
+        self.default_generation_config = {
+            "num_predict": 200,
+            "top_k": 20,
+            "top_p": 0.95,
+            "temperature": 0.1,            
+        }
+        super().__init__(model_path)
+        
+    def _load_model(self, model_path):
+        try:
+            ollama.show(model_path)
+        except ollama.ResponseError as e:
+            print(f"[ERROR] {e}")
+            
+            if e.status_code == 404:
+                print(f"[INFO] Downloading the model: {model_path}...")
+                ollama.pull(model_path)
+        self.model = 1
+        print("[INFO] LLM service started...")
+                
+    def generate(self, prompt, **generation_config):
+        torch.cuda.empty_cache()
+        
+        generation_config = self.default_generation_config
+        
+        if self.tokenize_with_chat_template:
+            response = chat(
+                model='phi3', 
+                messages=[
+                    {
+                        'role': 'user',
+                        'content': prompt,
+                    },
+                ],
+                keep_alive=self.keep_alive,
+                options=generation_config
+            )
+        else:
+            response = generate(
+                model='phi3',
+                prompt=prompt,
+                keep_alive=self.keep_alive,
+                options=generation_config 
+            )
+        result = response['message']['content']
+        torch.cuda.empty_cache()
+        
+        return result
