d4d57577f86f6a8a0ef6574765e41a6a597ac08c
Kasun <iamkasunimesha@gmail.com>
2024-08-12 15:35:24 +0530

    removed torch_dtype parameter from whisper stt

--- compute_service/src/stt/whisper/stt.py
+++ compute_service/src/stt/whisper/stt.py
@@ -1,5 +1,3 @@
-import os
-import torch
 import whisper
 import numpy as np
 from typing import Tuple
@@ -8,11 +6,11 @@ from src.stt.stt_base import STTBase
 
 
 class STT(STTBase):
-    def __init__(self, model_path: str, device: str="cpu", torch_dtype: torch.dtype=torch.float32, **model_kwargs):
+    def __init__(self, model_path: str, device: str="cpu", **model_kwargs):
         self.model = None
-        super().__init__(model_path, device, torch_dtype, **model_kwargs)
+        super().__init__(model_path, device, **model_kwargs)
 
-    def _load_model(self, model_path: str, device: str, torch_dtype: torch.dtype, **model_kwargs) -> None:
+    def _load_model(self, model_path: str, device: str, **model_kwargs) -> None:
         self.model = whisper.load_model(model_path, device=device)
 
     def transcribe(self, audio_array: np.ndarray, **generation_config: dict) -> str:
