aa11e22b556437f659de7772a1b76fbf05ee61a0
Kasun <iamkasunimesha@gmail.com>
2024-08-15 23:58:42 +0530

    added bot backend stt and tts classes

---
+++ bot_backend/src/stt.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+
+class STT:
+    def __init__(self):
+        self.url = os.getenv("STT_URL")
+        
+    def transcribe(self, audio_data: list, sample_rate: int, src_lang: str=None, tgt_lang: str=None) -> str:
+        json_body = {
+            "audio_data": audio_data,
+            "sample_rate": sample_rate
+        }
+        response = requests.post(self.url, json=json_body)
+        response = response.json()
+
+        return response["transcription"], response["language"]
+    
+    def is_ready(self) -> bool:
+        try:
+            response = requests.get(os.getenv("STATUS_URL"))
+            status = response.json()["stt"]
+        except:
+            status = False
+            
+        return status
---
+++ bot_backend/src/tts.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import numpy as np
+from typing import Tuple
+
+
+class TTS:
+    def __init__(self):
+        self.url = os.getenv("TTS_URL")
+        
+    def synthesize(self, text: str, language: str=None) -> Tuple[int, np.ndarray]:
+        json_body = {
+            "text": text,
+            "language": language
+        }
+        response = requests.post(self.url, json=json_body)
+        response = response.json()
+
+        audio_array = np.array(response["audio_response"])
+        sample_rate = response["sample_rate"]
+        
+        return sample_rate, audio_array
+    
+    def is_ready(self) -> bool:
+        try:
+            response = requests.get(os.getenv("STATUS_URL"))
+            status = response.json()["tts"]
+        except:
+            status = False
+            
+        return status
